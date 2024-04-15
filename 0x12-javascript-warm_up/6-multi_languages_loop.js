9aa81b617dd25bca4d9285dfa9ae4799558ece55
Simangeletutubala <simangeletutubala@gmail.com>
2024-04-15 12:33:46 -0700

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
-for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i]);
+const arrayStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+for (i; i < arrayStrings.length; i++) {
+  console.log(arrayStrings[i]);
 }
