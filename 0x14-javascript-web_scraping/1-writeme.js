d8efcad160b2f923859ce0325086924e1608d7f7
Simangeletutubala <simangeletutubala@gmail.com>
2024-05-25 15:48:10 -0700

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
 
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('File has been written');
-});
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
